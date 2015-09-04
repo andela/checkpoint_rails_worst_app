00f91265e1af5105c325b3e38367d11974ac10b1
Jeffrey Wan <Jwan622@gmail.com>
2015-09-04 11:39:41 +0100

    adding some more terrible AR queries

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -4,6 +4,7 @@ class StatsController < ApplicationController
     @prolific_author = Author.most_prolific_writer
     @author_with_most_upvoted_article = Author.with_most_upvoted_article
     @article_names = Article.all_names
+    @short_articles = Article.articles_with_names_less_than_20_char
 
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -9,6 +9,18 @@ class Article < ActiveRecord::Base
   end
 
   def self.five_longest_article_names
-    limit(5).order("(LENGTH(name)) desc").pluck(:name)
+    # limit(5).order("(LENGTH(name)) desc").pluck(:name)
+    all.sort_by do |art|
+      art.name
+    end.last(5).map do |art|
+      art.name
+    end
+  end
+
+  def self.articles_with_names_less_than_20_char
+    # where("length(name) < 20")
+    select do |art|
+      art.name.length < 20
+    end
   end
 end
