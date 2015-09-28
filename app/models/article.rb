278c4ed555c6f12a9507071b912527972a2ecd7e
Jeffrey Wan <Jwan622@gmail.com>
2015-09-28 14:49:01 +0100

    removing AR

--- app/models/article.rb
+++ app/models/article.rb
@@ -10,7 +10,6 @@ class Article < ActiveRecord::Base
   end
 
   def self.five_longest_article_names
-    # limit(5).order("(LENGTH(name)) desc").pluck(:name)
     all.sort_by do |art|
       art.name
     end.last(5).map do |art|
@@ -19,7 +18,6 @@ class Article < ActiveRecord::Base
   end
 
   def self.articles_with_names_less_than_20_char
-    # where("length(name) < 20")
     select do |art|
       art.name.length < 20
     end
